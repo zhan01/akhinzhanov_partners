345588637ee5dcf1b74a2de07001fcc2c5a554d2
zhan <zhunussov1994@gmail.com>
2019-11-12 20:13:57 +0600

    try to deploy 2

--- app.py
+++ app.py
@@ -12,4 +12,6 @@ def law():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
