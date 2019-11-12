b3a840eb9e837f5af4ae94fd0ee8ae7106e9d1e8
zhan <zhunussov1994@gmail.com>
2019-11-12 12:47:49 +0600

    make it better with python

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+
+app=Flask(__name__)
+
+
+    
+@app.route("/")
+def law():
+    return render_template('index.html', title='Law')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
