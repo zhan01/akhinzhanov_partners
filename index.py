2a8a909abc506f5da717cd1e73ad841a7cbcc6ca
zhan <zhunussov1994@gmail.com>
2019-11-28 12:48:26 +0600

    new added 2 pages

--- index.py
+++ index.py
@@ -9,7 +9,13 @@ app=Flask(__name__)
 def law():
     return render_template('index.html', title='Law')
 
+@app.route("/representation")
+def second():
+    return render_template('secondpage.html', title='Law')
 
+@app.route("/outsourcing")
+def third():
+    return render_template('thirdpage.html', title='Law')
 
 if __name__ == '__main__':
     app.run(debug=True)
